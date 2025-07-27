ec691ce9509383a32585642ca5fa22efee97b2ea
Rajdeep Singh <radewala.2@gmail.com>
2025-07-27 13:15:26 +0530

    about page

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="container h-96  justify-center  p-5 flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <div className="container p-10 bg-accent mb-28 flex flex-col mx-auto">
+        <h1 className="text-4xl text-center font-bold">About us</h1>
+      </div>
+    </main>
+  );
+}
