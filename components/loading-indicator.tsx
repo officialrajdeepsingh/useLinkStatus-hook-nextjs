0c3b0a61377ec6691100521b268c8d7fe3bd3322
Rajdeep Singh <radewala.2@gmail.com>
2025-07-27 13:17:40 +0530

    built a loading indicator for link component

---
+++ components/loading-indicator.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { Loader2Icon } from "lucide-react";
+import { useLinkStatus } from "next/link";
+
+export default function LoadingIndicator() {
+  const { pending } = useLinkStatus();
+  return pending ? <Loader2Icon className="animate-spin" /> : null;
+}
