889c84f04045f06fd19032e510598709adc0ff06
Rajdeep Singh <radewala.2@gmail.com>
2025-07-27 17:42:49 +0530

    remove the setTimeout function

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,15 @@
 "use client";
+
 import Link from "next/link";
 import LoadingIndicator from "./loading-indicator";
 import { CircleUserRound, HomeIcon, NotebookPen } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
-function onDelay() {
-  setTimeout(() => {
-    console.log("Delayed");
-  }, 6000);
-}
 export function Header() {
   return (
     <header className="container flex justify-center items-center p-4">
       <div className="max-w-2xl flex gap-5 mx-auto">
         <Link
-          onClick={onDelay}
           href="/"
           className="hover:underline hover:underline-offset-2"
           prefetch={false}
@@ -26,7 +21,6 @@ export function Header() {
           </Button>
         </Link>
         <Link
-          onClick={onDelay}
           href="/posts"
           className="hover:underline hover:underline-offset-2"
           prefetch={false}
@@ -38,7 +32,6 @@ export function Header() {
           </Button>
         </Link>
         <Link
-          onClick={onDelay}
           href="/about"
           className="hover:underline hover:underline-offset-2"
           prefetch={false}
